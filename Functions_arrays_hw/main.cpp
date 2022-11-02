7b4d486eda2e03146b4ce0f9b9bdaab710232a24
Peret_pu85@itstepru.onmicrosoft.com <Peret_pu85@itstepru.onmicrosoft.com>
2022-11-02 19:25:43 +0300

    Templated_Separated DONE

--- Functions_arrays_hw/main.cpp
+++ Functions_arrays_hw/main.cpp
@@ -1,10 +1,13 @@
-﻿#pragma warning(disable:4326)
-#include<iostream>
-#include<conio.h>
-using namespace std;
-
-const int ROWS = 3;
-const int COLS = 4;
+﻿#include"stdafx.h"
+#include"constants.h"
+#include"print.h"
+#include"print.cpp"
+#include"statistic.h"
+#include"statistic.cpp"
+#include"shift.h"
+#include"shift.cpp"
+#include"sort.h"
+#include"sort.cpp"
 
 void FillRand(int arr[], const int n);
 void FillRand(double arr[], const int n);
@@ -15,39 +18,6 @@ void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
 void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
 void FillRand(char arr[ROWS][COLS], const int ROWS, const int COLS);
 void FillRand(float arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-void Print(T arr[], const int n);
-template<typename T>
-void Print(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-int Sum(T arr[], const int n);
-template<typename T>
-int Sum(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-double Avg(T arr[], const int n);
-template<typename T>
-double Avg(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-void Sort(T arr[], const int n);
-
-template<typename T>
-int minValueIn(T arr[], const int n);
-template<typename T>
-int minValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-int maxValueIn(T arr[], const int n);
-template<typename T>
-int maxValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-void shiftLeft(T arr[], const int n, int number_of_shifts);
-template<typename T>
-void shiftRight(T arr[], const int n, int number_of_shifts);
 
 void shiftLeft(int arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
 
@@ -186,129 +156,6 @@ void FillRand(float arr[ROWS][COLS], const int ROWS, const int COLS)
 	}
 }
 
-template<typename T>
-void Print(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-template<typename T>
-void Print(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-}
-
-template<typename T>
-int Sum(T arr[], const int n)
-{
-	int sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-	}
-	return sum;
-}
-template<typename T>
-int Sum(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	int sum = 0;
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			sum += arr[i][j];
-		}
-	}
-	return sum;
-}
-
-template<typename T>
-double Avg(T arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-template<typename T>
-double Avg(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	return (double)Sum(arr, ROWS, COLS) / (ROWS * COLS);
-}
-
-template<typename T>
-int minValueIn(T arr[], const int n)
-{
-	int min = arr[0];
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-template<typename T>
-int minValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	int min = arr[0][0];
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			if (arr[i][j] < min)min = arr[i][j];
-		}
-	}
-	return min;
-}
-
-template<typename T>
-int maxValueIn(T arr[], const int n)
-{
-	int max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
-template<typename T>
-int maxValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	int max = arr[0][0];
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			if (arr[i][j] > max)max = arr[i][j];
-		}
-	}
-	return max;
-}
-
-template<typename T>
-void shiftLeft(T arr[], const int n, int number_of_shifts)
-{
-	for (int i = 0; i < number_of_shifts; i++)
-	{
-		int buffer = arr[0];
-		for (int i = 0; i < n; i++)
-		{
-			arr[i] = arr[i + 1];
-		}
-		arr[n - 1] = buffer;
-	}
-}
-template<typename T>
-void shiftRight(T arr[], const int n, int number_of_shifts)
-{
-	shiftLeft(arr, n, n - number_of_shifts);
-}
 
 void shiftLeft(int arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts)
 //
@@ -327,21 +174,3 @@ void shiftLeft(int arr[ROWS][COLS], const int ROWS, const int COLS, int number_o
 	}
 }
 
-template<typename T>
-void Sort(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			//arr[i] - выбранный элемент;
-			//arr[j] - перебираемый элемент
-			if (arr[j] < arr[i])
-			{
-				int buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
---
+++ Functions_arrays_hw/stdafx.h
@@ -0,0 +1,5 @@
+#pragma once
+#pragma warning(disable:4326)
+#include<iostream>
+#include<conio.h>
+using namespace std;
